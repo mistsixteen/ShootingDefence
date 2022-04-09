46c195e1c4d356cc4e9cfc393f218e9d1024d272
mistsixteen <mist.sixteen@gmail.com>
2022-04-10 00:56:48 +0900

    Camera Movement 구현 : Player 따라오도록

--- ShootingDefence/Assets/Scripts/CameraController.cs
+++ ShootingDefence/Assets/Scripts/CameraController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform pTransform;
+    float camOffsetZ;
+    private Coroutine c_followCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //m_player = GameObject.Find("Player");
+        camOffsetZ = gameObject.transform.position.z - pTransform.position.z;
+        c_followCamera = StartCoroutine(followCamera());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator followCamera()
     {
+        while(true)
+        {
+            Vector3 m_cameraPos = new Vector3(pTransform.position.x, gameObject.transform.position.y, pTransform.position.z + camOffsetZ);
+
+            gameObject.transform.position = m_cameraPos;
+
+            yield return new WaitForSeconds(0.01f);
+        }
         
     }
+    
 }

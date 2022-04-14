811276cd69c3c381958cbf77c6a84aad76c753ca
mistsixteen <mist.sixteen@gmail.com>
2022-04-15 08:22:49 +0900

    카메라가 보는 방향쪽으로 조금 더 이동하도록 구현

--- ShootingDefence/Assets/Scripts/CameraController.cs
+++ ShootingDefence/Assets/Scripts/CameraController.cs
@@ -5,35 +5,50 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform pTransform;
+    public Camera cam;
+    
     public float minYOffset = 10.0f;
     public float maxYOffset = 20.0f;
     private float currentYOffset = 18.0f;
-    private float camOffsetZ;
+
+    public float threshold = 5.0f;
+
 
     private Coroutine c_followCamera;
     // Start is called before the first frame update
     void Start()
     {
-        //m_player = GameObject.Find("Player");
-        camOffsetZ = gameObject.transform.position.z - pTransform.position.z;
-        c_followCamera = StartCoroutine(followCamera());
+        c_followCamera = StartCoroutine(CameraRoutine());
     }
 
-    IEnumerator followCamera()
+    IEnumerator CameraRoutine()
     {
+        Vector3 screenPosition = new Vector3();
+        Vector3 targetPosition;
+
         while(true)
         {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                screenPosition = new Vector3(hit.point.x, 0.0f, hit.point.z);
+                targetPosition = (pTransform.position + screenPosition) / 2.0f;
+
+                targetPosition.x = Mathf.Clamp(targetPosition.x, pTransform.position.x - threshold, pTransform.position.x + threshold);
+                targetPosition.z = Mathf.Clamp(targetPosition.z, pTransform.position.z - threshold, pTransform.position.z + threshold);
+            }
+            else // Raycast 실패시 Camera 위치 유지
+                targetPosition = transform.position;
+
             currentYOffset += Input.mouseScrollDelta.y;
-            if (currentYOffset < minYOffset)
-                currentYOffset = minYOffset;
-            if (currentYOffset > maxYOffset)
-                currentYOffset = maxYOffset;
+            currentYOffset = Mathf.Clamp(currentYOffset, minYOffset, maxYOffset);
+            targetPosition.y = currentYOffset;
 
-            Vector3 m_cameraPos = new Vector3(pTransform.position.x, currentYOffset, pTransform.position.z + camOffsetZ);
+            gameObject.transform.position = targetPosition;
 
-            gameObject.transform.position = m_cameraPos;
-
-            yield return new WaitForSeconds(0.01f);
+            yield return new WaitForSeconds(0.03f);
         }
         
     }

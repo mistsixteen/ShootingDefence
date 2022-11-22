77e3ff8386418f762b169b7a4f4020df5fde40ee
mistsixteen <mist.sixteen@gmail.com>
2022-11-22 18:31:20 +0900

    add class Appinstance / Change Scene Management
    Appinstance class 추가 / Scene 관리 관련 함수 추가

---
+++ ShootingDefence/Assets/Scripts/App/AppInstance.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class AppInstance : MonoBehaviour
+{
+    private static AppInstance instance;
+
+    public ModelManager ModelManager;
+    public TableManager TableManager;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+        instance = this;
+
+        this.ModelManager = new ModelManager();
+        this.TableManager = new TableManager();
+    }
+
+    void Start()
+    {
+        ChangeCurrentScene(EnumGameScene.SceneGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public static AppInstance GetInstance(){
+        return instance;
+    }
+
+    public void ChangeCurrentScene(EnumGameScene targetScene)
+    {
+        switch (targetScene)
+        {
+            case EnumGameScene.SceneGameStart:
+                break;
+            case EnumGameScene.SceneMainMenu:
+                break;
+            case EnumGameScene.SceneGameReady:
+                break;
+            case EnumGameScene.SceneGame:
+                SceneManager.LoadScene("SceneGameMode");
+                break;
+            case EnumGameScene.SceneGameOver:
+                break;
+        }
+    }
+}

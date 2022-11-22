ecb4872e359f0744481ccbe911b8a52e2ef4ffef
mistsixteen <mist.sixteen@gmail.com>
2022-11-22 18:56:01 +0900

    BulletFactory 구조 수정
    BulletFactory 구조 수정(Object Pooling용)

--- ShootingDefence/Assets/Scripts/GameScene/BulletFactory.cs
+++ ShootingDefence/Assets/Scripts/GameScene/BulletFactory.cs
@@ -2,35 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletFactory
+public class BulletFactory : MonoBehaviour
 {
-    private static BulletFactory instance;
 
-    GameObject bulletGameObject = null;
+    private static BulletFactory instance;
+    private Queue<Bullet> bulletObjectPool;
+    private GameObject bulletGameObject = null;
 
     public static BulletFactory GetInstance()
     {
-        if(instance == null)
-        {
-            instance = new BulletFactory();
-        }
         return instance;
     }
 
-    public BulletFactory()
+    private void Awake()
     {
+        instance = this;
         bulletGameObject = Resources.Load<GameObject>("Prefabs/Bullet");
+        bulletObjectPool = new Queue<Bullet>();
+    }
+
+    public void EnQueueBullet(Bullet bulletObject)
+    {
+        if (bulletObject){
+            bulletObjectPool.Enqueue(bulletObject);
+        }
     }
 
     public GameObject CreateBullet(in BulletInfo bInfo, in Vector3 bulletPos, in Quaternion bulletRot)
     {
-        GameObject newObject = GameObject.Instantiate(bulletGameObject, bulletPos, bulletRot);
+        GameObject newObject = Instantiate(bulletGameObject, bulletPos, bulletRot, transform);
         if(newObject == null)
         {
-            Debug.Log("NULL EXEP");
+            return null;
         }
         newObject.GetComponent<Bullet>().RegisterBulletInfo(bInfo);
-
         return newObject;
     }
 

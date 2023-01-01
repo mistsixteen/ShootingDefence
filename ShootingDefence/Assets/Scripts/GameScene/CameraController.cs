a42e9c9110107021906841b696dc266f47366448
mistsixteen <mist.sixteen@gmail.com>
2023-01-02 02:17:17 +0900

    Camera Controller를 Coroutine -> Update로 변경 : 게임을 멈추었을 때 카메라도 멈춰있도록

--- ShootingDefence/Assets/Scripts/GameScene/CameraController.cs
+++ ShootingDefence/Assets/Scripts/GameScene/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -13,44 +14,36 @@ public class CameraController : MonoBehaviour
 
     public float threshold = 5.0f;
 
-
-    private Coroutine c_followCamera;
     // Start is called before the first frame update
     void Start()
     {
-        c_followCamera = StartCoroutine(CameraRoutine());
     }
 
-    IEnumerator CameraRoutine()
+    private void Update()
     {
         Vector3 screenPosition = new Vector3();
         Vector3 targetPosition;
 
-        while(true)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //마우스 포인터의 위치를 Raycast하여, 마우스 위치 - 캐릭터 위치의 중간값으로 카메라 이동
+        if (Physics.Raycast(ray, out hit))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            screenPosition = new Vector3(hit.point.x, 0.0f, hit.point.z);
+            targetPosition = (pTransform.position + screenPosition) / 2.0f;
 
-            //마우스 포인터의 위치를 Raycast하여, 마우스 위치 - 캐릭터 위치의 중간값으로 카메라 이동
-            if (Physics.Raycast(ray, out hit))
-            {
-                screenPosition = new Vector3(hit.point.x, 0.0f, hit.point.z);
-                targetPosition = (pTransform.position + screenPosition) / 2.0f;
-
-                targetPosition.x = Mathf.Clamp(targetPosition.x, pTransform.position.x - threshold, pTransform.position.x + threshold);
-                targetPosition.z = Mathf.Clamp(targetPosition.z, pTransform.position.z - threshold, pTransform.position.z + threshold);
-            }
-            else // Raycast 실패시 Camera 위치 유지
-                targetPosition = transform.position;
-            //카메라 높이는 Wheel로 수정
-            currentYOffset += Input.mouseScrollDelta.y;
-            currentYOffset = Mathf.Clamp(currentYOffset, minYOffset, maxYOffset);
-            targetPosition.y = currentYOffset;
-
-            gameObject.transform.position = targetPosition;
-
-            yield return new WaitForSeconds(0.01f);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, pTransform.position.x - threshold, pTransform.position.x + threshold);
+            targetPosition.z = Mathf.Clamp(targetPosition.z, pTransform.position.z - threshold, pTransform.position.z + threshold);
         }
-        
+        else // Raycast 실패시 Camera 위치 유지
+            targetPosition = transform.position;
+        //카메라 높이는 Wheel로 수정
+        currentYOffset += Input.mouseScrollDelta.y;
+        currentYOffset = Mathf.Clamp(currentYOffset, minYOffset, maxYOffset);
+        targetPosition.y = currentYOffset;
+
+        gameObject.transform.position = targetPosition;
     }
+
 }
